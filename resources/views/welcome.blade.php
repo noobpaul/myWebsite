a2fe8594df896865a772126855a3344f63f45a60
noobpaul <paulgenesisbacuna@gmail.com>
2017-05-14 15:18:11 +0800

    Changed Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MyWebsite
                 </div>
 
                 <div class="links">
