cd040a0bc2597e665d564db6cca55dfcd10f395b
noobpaul <paulgenesisbacuna@gmail.com>
2017-05-14 16:02:40 +0800

    Added new View

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+	public function create()
+	{
+		return view('CRUD.create');
+	}
+}
+
+
+ ?>
---
+++ resources/views/CRUD/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('content')
+	<form>
+		<input type="text" name="firstname"><br>
+		<button type="submit">Create</button>
+	</form>
+@stop
